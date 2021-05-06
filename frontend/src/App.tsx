5f35080b7b63129c75c7d17e515de7ef07736324
rogerplis <rogerplis@hotmail.com>
2021-05-06 19:26:34 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 import logo from './logo.svg';
 
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <div className="App">
-      <h1>Ola Mundo</h1>     
-    </div>
+      <h1 className="text-primary"  >Ola Mundo</h1>   
+      <DataTable/>  
+        </div>
+        <Footer/>        
+    </>
   );
 }
 
