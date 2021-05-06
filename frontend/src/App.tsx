010cb000250e19d68325d6b65f9a4b425874ebd0
rogerplis <rogerplis@hotmail.com>
2021-05-06 19:54:41 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,33 @@
-import DataTable from 'components/DataTable';
-import Footer from 'components/Footer';
-import NavBar from 'components/NavBar';
-import React from 'react';
-import logo from './logo.svg';
-
+import BarChart from "components/BarCharts";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <div className="App">
-      <h1 className="text-primary"  >Ola Mundo</h1>   
-      <DataTable/>  
+      <NavBar />
+      <div className="App">
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5>Taxa de retorno</h5>
+            <BarChart/>
+          </div>
+          <div className="col-sm-6">
+            <h5>Taxa de retorno</h5>
+            <DonutChart/>
+          </div>
         </div>
-        <Footer/>        
+    <div className="py-3">
+      <h3 className="text-secondary" >Todas Vendas</h3>
+    </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
