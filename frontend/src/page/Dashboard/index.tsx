91817a238c7fa840243f674cc827e61f5a5b9930
rogerplis <rogerplis@hotmail.com>
2021-05-08 16:43:40 -0300

    Correcao de Layout

--- frontend/src/page/Dashboard/index.tsx
+++ frontend/src/page/Dashboard/index.tsx
@@ -9,7 +9,7 @@ function Dashboard() {
     return (
         <>
       <NavBar />
-      <div className="content">
+      <div className="container">
         <h1 className="text-primary py-3">DashBoard de Vendas</h1>
         <div className="row px-3">
           <div className="col-sm-6">
